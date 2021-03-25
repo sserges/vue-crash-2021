6ce1e4e95a131836a6d2c935bc86f3901c1212ec
sserges <agounkeserge@gmail.com>
2021-03-25 23:56:44 +0000

    Events

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="btn" :style="{ background: color }">{{ text }}</button>
+  <button @click="onClick" class="btn" :style="{ background: color }">
+    {{ text }}
+  </button>
 </template>
 
 <script>
@@ -8,6 +10,11 @@ export default {
   props: {
     text: String,
     color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('click')
+    },
   },
 }
 </script>
