f2d8249cefad5cbd529ee22c8b1b0373aadfe58f
sserges <agounkeserge@gmail.com>
2021-03-29 20:48:17 +0000

    Task Data & created() Method

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,33 @@ export default {
   components: {
     Header,
   },
+  data() {
+    return {
+      tasks: [],
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3rd at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 3rd at 11:00am',
+        reminder: false,
+      },
+    ]
+  },
 }
 </script>
 
