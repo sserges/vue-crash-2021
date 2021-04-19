7356bb63ee307b9bcf4e909c1bb362d8793cefaf
sserges <agounkeserge@gmail.com>
2021-04-19 16:27:33 +0000

    Tasks Component & v-for Loop

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
   },
   data() {
     return {
---
+++ src/components/Task.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    }
+}
+</script>
