f76e67860d2bd654b997765af5ad23b37dd92935
sserges <agounkeserge@gmail.com>
2021-05-08 02:35:28 +0000

    AddTask Component & v-model

---
+++ src/components/AddTask.vue
@@ -0,0 +1,95 @@
+<template>
+  <form @submit.prevent="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input
+        type="text"
+        v-model.trim="text"
+        name="text"
+        placeholder="Add Task"
+        required
+      />
+    </div>
+    <div class="form-control">
+      <label>Day & Time</label>
+      <input
+        type="datetime-local"
+        v-model.trim="day"
+        name="day"
+        placeholder="Add Day & Time"
+        required
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data() {
+    return {
+      text: '',
+      day: '',
+      reminder: '',
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (!this.text) {
+        alert('Please add a task')
+        return
+      }
+
+      const newTask = {
+        id: Date.now(),
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      }
+
+      this.$emit('add-task', newTask)
+
+      this.text = ''
+      this.day = ''
+      this.reminder = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+.form-control {
+  margin: 20px 0;
+}
+.form-control label {
+  display: block;
+}
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.form-control-check label {
+  flex: 1;
+}
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
