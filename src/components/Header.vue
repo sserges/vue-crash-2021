b51861b0954d11829ccf3f2db08bcf4792cb3f8f
sserges <agounkeserge@gmail.com>
2021-05-22 17:23:23 +0000

    Restrict a Component to a Route

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
   <header>
     <h1>{{ title }}</h1>
     <Button
+      v-if="homePage"
       @click="$emit('click')"
       :text="showAddTask ? 'Close' : 'Add Task'"
       :color="showAddTask ? 'red' : 'green'"
@@ -20,6 +21,15 @@ export default {
   },
   components: {
     Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === '/') {
+        return true
+      } else {
+        return false
+      }
+    },
   },
 }
 </script>
